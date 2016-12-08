8277ade176915569b5adef9a39f242754fc028ae
PlanetEfficacy <jspevack@gmail.com>
2016-12-07 17:28:59 -0700

    Update strategy

--- lib/omniauth/strategies/census.rb
+++ lib/omniauth/strategies/census.rb
@@ -1,13 +1,24 @@
-require 'omniauth/strategies/oauth2'
-
-module Omniauth
+require 'omniauth-oauth2'
+module OmniAuth
   module Strategies
-    class Census < Omniauth::Strategies::OAuth2
-      option :name, :census
+    class Learn < OmniAuth::Strategies::OAuth2
+      include OmniAuth::Strategy
       option :client_options, {
-        :site => "http://localhost:3000"
-        :authorize_url => "/oauth/authorize"
-      }
+               site: "https://localhost:3000",
+               authorize_url: "https://localhost:3000/oauth/authorize",
+               token_url: "https://localhost:3000/oauth/token"
+             }
+      def request_phase
+        super
+      end
+      info do
+        raw_info.merge("token" => access_token.token)
+      end
+      uid { raw_info["id"] }
+      def raw_info
+        @raw_info ||=
+          access_token.get('/api/v1/users/me').parsed
+      end
     end
   end
 end
