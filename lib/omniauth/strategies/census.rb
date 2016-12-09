76decdd2147839325b76e6038a1a8f2721164e9c
PlanetEfficacy <jspevack@gmail.com>
2016-12-08 17:20:52 -0700

    We are victorious: update callback and API path

--- lib/omniauth/strategies/census.rb
+++ lib/omniauth/strategies/census.rb
@@ -1,24 +1,34 @@
 require 'omniauth-oauth2'
+
 module OmniAuth
   module Strategies
     class Census < OmniAuth::Strategies::OAuth2
       include OmniAuth::Strategy
       option :client_options, {
-               site: "https://localhost:3000",
-               authorize_url: "https://localhost:3000/oauth/authorize",
-               token_url: "https://localhost:3000/oauth/token"
+               site: "http://localhost:3000",
+               authorize_url: "/oauth/authorize",
+               token_url: "/oauth/token"
              }
+
       def request_phase
         super
       end
+
+      def callback_url
+        full_host + script_name + callback_path
+      end
+
       info do
         raw_info.merge("token" => access_token.token)
       end
+
       uid { raw_info["id"] }
+
       def raw_info
         @raw_info ||=
-          access_token.get('/api/v1/users/me').parsed
+          access_token.get('/api/v1/me').parsed
       end
+
     end
   end
 end
